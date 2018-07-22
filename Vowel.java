09f2b24e3bc152135ff6901121f12d3e52f8c262
psaranya12 <41112337+psaranya12@users.noreply.github.com>
2018-07-22 11:05:32 +0530

    Add files via upload

--- Vowel.java
+++ Vowel.java
@@ -8,10 +8,13 @@ public static void main(String[] args) {
 		ch=in.next().charAt(0);
 		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 		{
-			System.out.println("The given charcter is vowel");
+			System.out.println("The given character is vowel");
 		}
-		else{
-			System.out.println("The given charcter is consonant");
+		else if(ch>='0'&&ch<='9'){
+			System.out.println("The given character is digit");
+		}
+			else{
+			System.out.println("The given character is consonant");
 		}
 	}
 
